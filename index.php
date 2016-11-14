ef1f7190c64b53cb78a34ef419dc3cedc929f594
af4n <aleksey.afanaskin@gmail.com>
2016-11-14 17:28:05 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	require_once 'header.php';
+
+	echo "<br><span class='main'>Welcome to $appname,";
+	// Добро пожаловать в ...
+	if ($loggedin) echo " $user, you are logged in.";
+	// вы вошли на сайт
+	else echo ' please sign up and/or log in to join in.';
+	// Пожалуйста, зарегистрируйтесь и (или) войдите на сайт
+?>
+		</span><br><br>
+	</body>
+</html>
